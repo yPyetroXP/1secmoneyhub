e795c047df68eecbfa92026ddb34e3812c1d1800
bIpOlAr <117316947+yPyetroXP@users.noreply.github.com>
2024-06-01 17:36:46 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,41 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GlobalEvents = ReplicatedStorage:WaitForChild("events-shared"):WaitForChild("network@GlobalEvents")
+
+local Window = Rayfield:CreateWindow({
+   Name = "1 SEC HUB",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "1 SEC HUB"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local Button = Window:CreateButton({
+   Name = "Button Example",
+   Callback = function()
+       -- O que acontecerá quando o botão for pressionado
+       while true do
+           GlobalEvents:WaitForChild("placeBlock"):FireServer()
+           wait() -- Aguarda um curto intervalo antes de continuar
+       end
+   end,
+})
